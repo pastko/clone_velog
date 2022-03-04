6aa7621462a4604284d4015252299a3e852c0b47
hwlee-study <hwlee8042@gmail.com>
2022-03-04 17:26:33 +0900

    remove old settings

--- glog-client/src/components/board/detail/Contents.js
+++ glog-client/src/components/board/detail/Contents.js
@@ -1,28 +1,19 @@
-import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
-import Images from "../../layout/Images";
+import styled from 'styled-components'
 
 function Contents() {
-    const dispatch = useDispatch();
-    const { posts, detailPost } = useSelector((state) => ({
-        posts: state.board.posts,
-        detailPost: state.board.detailPost
-    }));
-
-    return (
-        <StyledContents>
-        </StyledContents>
-        // <StyledContents dangerouslySetInnerHTML={{ __html: contents }}></StyledContents>
-    );
+  return (
+    <StyledContents></StyledContents>
+    // <StyledContents dangerouslySetInnerHTML={{ __html: contents }}></StyledContents>
+  )
 }
 
 const StyledContents = styled.div`
-    margin-bottom: 4rem;
-    p {
-        text-align: left;
-        margin-top: 50px;
-        font-size: 1.3rem;
-    }
-`;
+  margin-bottom: 4rem;
+  p {
+    text-align: left;
+    margin-top: 50px;
+    font-size: 1.3rem;
+  }
+`
 
-export default Contents;
+export default Contents
--- glog-client/src/pages/mypage/Profile.js
+++ glog-client/src/pages/mypage/Profile.js
@@ -1,50 +1,46 @@
-import styled from "styled-components";
-import ThumbnailArea from "../../components/mypage/ThumbnailArea";
-import UserInfo from "../../components/mypage/UserInfo";
-import { useSelector, useDispatch } from "react-redux";
+import styled from 'styled-components'
+import ThumbnailArea from '../../components/mypage/ThumbnailArea'
+import UserInfo from '../../components/mypage/UserInfo'
+import { useSelector, useDispatch } from 'react-redux'
 
 function Profile() {
-    const users = useSelector((state) => state.user.user);
-    return (
-        <StyledProfile>
-            <ThumbnailArea />
-            <StyledInputWrap>
-                <UserInfo
-                    title='굳로그 제목'
-                    contents={users.glogTitle}
-                    isButton='수정'
-                    description='개인 페이지의 좌측 상단에 나타나는 페이지 제목입니다.'
-                />
-                <UserInfo
-                    title='이메일 주소'
-                    contents={users.mail}
-                    isButton='none'
-                    description='회원 인증 또는 시스템에서 발송하는 이메일을 수신하는 주소입니다.'
-                />
-                <UserInfo
-                    title='회원 탈퇴'
-                    isButton='회원 탈퇴'
-                    description='탈퇴 시 작성하신 포스트 및 댓글이 모두 삭제되며 복구되지 않습니다.'
-                />
-            </StyledInputWrap>
-        </StyledProfile>
-    );
+  const users = useSelector((state) => state.user.user)
+  return (
+    <StyledProfile>
+      <ThumbnailArea />
+      <StyledInputWrap>
+        <UserInfo
+          title="제목"
+          isButton="수정"
+          description="개인 페이지의 좌측 상단에 나타나는 페이지 제목입니다."
+        />
+        <UserInfo
+          title="이메일 주소"
+          isButton="none"
+          description="회원 인증 또는 시스템에서 발송하는 이메일을 수신하는 주소입니다."
+        />
+        <UserInfo
+          title="회원 탈퇴"
+          isButton="회원 탈퇴"
+          description="탈퇴 시 작성하신 포스트 및 댓글이 모두 삭제되며 복구되지 않습니다."
+        />
+      </StyledInputWrap>
+    </StyledProfile>
+  )
 }
 const StyledProfile = styled.div`
-    margin: 4rem auto 0;
-    max-width: 768px;
-    @media (max-width: 1280px) {
-        width: 768px;
-    }
-    @media (max-width: 768px) {
-        width: 100vw;
-    }
-`;
+  margin: 4rem auto 0;
+  max-width: 768px;
+  @media (max-width: 1280px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100vw;
+  }
+`
 const StyledInputWrap = styled.div`
-    width: 100%;
-    margin-top: 3rem;
-`;
+  width: 100%;
+  margin-top: 3rem;
+`
 
-
-
-export default Profile;
+export default Profile
