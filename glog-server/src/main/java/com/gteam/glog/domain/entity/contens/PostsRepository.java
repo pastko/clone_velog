a23ba6fe07f37eb713ede5b01928882fcdffd5fc
pastko <storyofstory@naver.com>
2022-03-18 22:10:48 +0900

    fix: PageNation 및 Sort 기능 적용하여 기능에 맞게 수정

--- glog-server/src/main/java/com/gteam/glog/domain/entity/contens/PostsRepository.java
+++ glog-server/src/main/java/com/gteam/glog/domain/entity/contens/PostsRepository.java
@@ -1,19 +1,22 @@
 package com.gteam.glog.domain.entity.contens;
 
+import com.gteam.glog.domain.dto.PostsInfoDTO;
 import com.gteam.glog.domain.entity.users.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Posts,Long> {
-    Optional<Posts> findContentsByIdx(Long id);
-    Optional<Posts> findContentsByUsers(Users users);
-    Optional<Posts> findContentsByUsers(Users users, Sort sort);
-    Optional<Posts> findContentsByUsers(Users users,Pageable pageable);
+    Optional<List<Posts>> findPostsByUsers(Users users);
+    Optional<Posts> findPostsByIdx(Long id);
+    Optional<List<Posts>> findPostsByUsers(Users users, Sort sort);
+    Optional<Page<Posts>> findPostsByUsers_Idx(Long idx, Pageable pageable);
     Page<Posts> findAll(Pageable pageable);
 }
